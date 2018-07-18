d8a0d246ab4f65f98069125ddfc8d7c0e01ea0b9
Michael Carroll <carrollm2@gmail.com>
2018-07-18 02:28:16 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,10 +10,20 @@ function frameIt(){
 };
 
 
+function pressIt(){
+  $(document).on('keydown', function(key){
+    if (key.which == 71){
+      alert('G was pressed');
+    }
+  });
+};
+
 $(document).ready(function(){
 
   getIt();
 
   frameIt();
+
+  pressIt();
 
 });
