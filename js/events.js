178e4cfbf341055069656c16f7a63ad227c69956
Michael Carroll <carrollm2@gmail.com>
2018-07-18 02:23:30 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,16 @@
 //define functions here
+function getIt(){
+  $('p').on('click').function(){
+      alert('Hey!');
+  });
+};
+
 
 $(document).ready(function(){
 
 // call functions here
+//$("p").click();
+  $('p').bind('click', function(){});
+  $('p').trigger('click');
 
 });
